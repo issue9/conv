476e236373c120e0ce7025b5f9116cc86a542de7
Matt Ma <dayed.hell@gmail.com>
2020-05-18 10:15:32 +0800

    Must*可省略参数

--- conv_test.go
+++ conv_test.go
@@ -22,6 +22,11 @@ func TestMustBool(t *testing.T) {
 	a.False(MustBool("off", true))
 	a.False(MustBool(false, true))
 
+	a.True(MustBool("1"))
+	a.True(MustBool("true"))
+	a.False(MustBool("0"))
+	a.False(MustBool("false"))
+
 	// 不可解析
 	a.False(MustBool("str", false))
 	a.True(MustBool(";adf", true))
@@ -41,6 +46,7 @@ func TestMustInt(t *testing.T) {
 	a.Equal(MustInt(1.23, 99), 1)
 	a.Equal(MustInt(-1.23, 99), -1)
 	a.Equal(MustInt([]byte("123"), 44), 123)
+	a.Equal(MustInt("1"), 1)
 
 	// 不可解析
 	a.Equal(MustInt(";sdf", 45), 45)
@@ -61,6 +67,7 @@ func TestMustInt64(t *testing.T) {
 	a.Equal(MustInt64(1.23, 99), int64(1))
 	a.Equal(MustInt64(-1.23, 99), int64(-1))
 	a.Equal(MustInt64([]byte("123"), 44), int64(123))
+	a.Equal(MustInt64("1"), 1)
 
 	// 不可解析
 	a.Equal(MustInt64(";sdf", 45), int64(45))
@@ -78,6 +85,7 @@ func TestMustUint32(t *testing.T) {
 	a.Equal(MustUint32(uint64(33), 99), uint32(33))
 	a.Equal(MustUint32(1.23, 99), uint32(1))
 	a.Equal(MustUint32([]byte("123"), 44), uint32(123))
+	a.Equal(MustUint32("1"), 1)
 
 	// 不可解析
 	a.Equal(MustUint32(int64(-123), 99), uint32(99))
@@ -98,6 +106,7 @@ func TestMustUint64(t *testing.T) {
 	a.Equal(MustUint64(uint64(33), 99), uint64(33))
 	a.Equal(MustUint64(1.23, 99), uint64(1))
 	a.Equal(MustUint64([]byte("123"), 44), uint64(123))
+	a.Equal(MustUint64("1"), 1)
 
 	// 不可解析
 	a.Equal(MustUint64(int64(-123), 99), uint64(99))
@@ -117,6 +126,7 @@ func TestMustFloat32(t *testing.T) {
 	a.Equal(MustFloat32(uint(8), 9), float32(8))
 	a.Equal(MustFloat32(uint64(33), 99), float32(33))
 	a.Equal(MustFloat32([]byte("123"), 44), float32(123))
+	a.Equal(MustFloat32("1"), 1)
 
 	// 不可解析
 	a.Equal(MustFloat32(";sdf", 45), 45)
@@ -130,6 +140,7 @@ func TestMustString(t *testing.T) {
 	a.Equal(MustString(-11, "22"), "-11")
 	a.Equal(MustString(-11.111, "22"), "-11.111")
 	a.Equal(MustString(true, "22"), "true")
+	a.Equal(MustString(1), "1")
 
 	// 不可解析
 	a.Equal(MustString([]int{1}, "22"), "22")
@@ -141,6 +152,7 @@ func TestMustBytes(t *testing.T) {
 	a.Equal(MustBytes("123", []byte("456")), []byte{49, 50, 51})
 	a.Equal(MustBytes(123, []byte("456")), []byte{49, 50, 51})
 	a.Equal(MustBytes(uint(123), []byte("456")), []byte{49, 50, 51})
+	a.Equal(MustBytes(123), []byte("123"))
 
 	// 不可解析
 	a.Equal(MustBytes([]int{1}, []byte("123")), []byte{49, 50, 51})
@@ -157,6 +169,8 @@ func TestMustSlice(t *testing.T) {
 
 	a.Equal(MustSlice([]byte("123"), def), []interface{}{byte(49), byte(50), byte(51)})
 	a.Equal(MustSlice("123", def), []interface{}{rune(49), rune(50), rune(51)})
+	a.Equal(MustSlice([]int{1, 2, 3}), []interface{}{int(1), int(2), int(3)})
+	a.Equal(MustSlice([]uint64{1, 2, 3}), []interface{}{uint64(1), uint64(2), uint64(3)})
 
 	// 不可解析
 	a.Equal(MustSlice(7, def), []interface{}{4, 5, 5})
